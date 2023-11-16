a6b161f27642364d138d0540bc9a554b24482a15
Freddy Rondon <frondonfermin@gmail.com>
2023-11-15 23:09:20 -0400

    Fix budget progress bar styling

--- expense-tracker-frontend/src/components/BudgetProgressBar.js
+++ expense-tracker-frontend/src/components/BudgetProgressBar.js
@@ -1,8 +1,23 @@
-// BudgetProgressBar.js
 import React from 'react';
 
 const BudgetProgressBar = ({ budget }) => {
-  const percentageSpent = ((budget.amountSpent / budget.amount) * 100).toFixed(2);
+  const rawPercentageSpent = (budget.amountSpent / budget.amount) * 100;
+  const percentageSpent = rawPercentageSpent.toFixed(2); // Actual percentage
+  const visualPercentageSpent = Math.min(rawPercentageSpent, 100).toFixed(2); // Visual cap at 100%
+
+  // Function to determine color based on the raw percentage spent
+  const getProgressBarColor = (percentage) => {
+    // If the percentage is over 100, always return red
+    if (percentage > 100) {
+      return 'hsl(0, 100%, 50%)'; // Red
+    }
+  
+    // Calculate the hue based on the percentage
+    // Start with green (120) and end with red (0)
+    const hue = ((100 - percentage) * 120 / 100);
+    return `hsl(${hue}, 100%, 50%)`;
+  };
+  
 
   return (
     <div className="bg-white rounded shadow p-4 mb-4">
@@ -12,8 +27,11 @@ const BudgetProgressBar = ({ budget }) => {
       </div>
       <div className="w-full bg-gray-200 rounded-full h-2.5 mb-2">
         <div
-          className="bg-blue-600 h-2.5 rounded-full"
-          style={{ width: `${percentageSpent}%` }}
+          className="h-2.5 rounded-full"
+          style={{ 
+            width: `${visualPercentageSpent}%`, 
+            backgroundColor: getProgressBarColor(rawPercentageSpent),
+          }}
         ></div>
       </div>
       <div className="text-sm font-semibold text-gray-700">
